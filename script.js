982faf295e126658a2500bfb3e6d020f750ee448
trythebest <manikandan.nagarajan.1998@gmail.com>
2023-12-21 14:06:33 +0530

    intialize

---
+++ script.js
@@ -0,0 +1,60 @@
+let num = 10;
+var count = document.createElement("div");
+count.innerHTML = num;
+document.body.append(count);
+
+setTimeout(() => {
+    num--;
+    count.innerHTML = num;
+    setTimeout(() => {
+        num--;
+        count.innerHTML = num;
+        setTimeout(() => {
+            num--;
+            count.innerHTML = num;
+            setTimeout(() => {
+                num--;
+                count.innerHTML = num;
+                setTimeout(() => {
+                    num--;
+                    count.innerHTML = num;
+                    setTimeout(() => {
+                        num--;
+                        count.innerHTML = num;
+                        setTimeout(() => {
+                            num--;
+                            count.innerHTML = num;
+                            setTimeout(() => {
+                                num--;
+                                count.innerHTML=num;
+                                setTimeout(() => {
+                                    num--;
+                                    count.innerHTML=num;
+                                    setTimeout(() => {
+                                        num--;
+                                        count.innerHTML=num;
+                                        setTimeout(() => {
+                                            num--;
+                                            count.innerHTML="HAPPY INDEPENDENCE DAY";
+                                            
+                                        },1000);
+                                        
+                                    },1000);
+                                    
+                                },1000);
+                                
+                            },1000);
+
+                        }, 1000);
+
+                    }, 1000);
+
+                }, 1000);
+
+            }, 1000);
+
+        }, 1000);
+
+    }, 1000);
+
+}, 1000);
